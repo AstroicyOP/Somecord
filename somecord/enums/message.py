be59e810ff3f5e2b3d2467f475b254284bc434b7
Astroicy <astroicy.gabriel@gmail.com>
2022-08-15 14:19:44 +0200

    Somecord
    Somecord

---
+++ somecord/enums/message.py
@@ -0,0 +1,64 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2022-present EmreTech
+Copyright (c) 2022-present Astroicy
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+from enum import Enum
+
+__all__ = (
+    "MessageActivityType",
+    "MessageType",
+)
+
+
+class MessageActivityType(Enum):
+    JOIN = 1
+    SPECTATE = 2
+    LISTEN = 3
+    JOIN_REQUEST = 5
+
+
+class MessageType(Enum):
+    DEFAULT = 0
+    RECIPIENT_ADD = 1
+    RECIPIENT_REMOVE = 2
+    CALL = 3
+    CHANNEL_NAME_CHANGE = 4
+    CHANNEL_ICON_CHANGE = 5
+    CHANNEL_PINNED_MESSAGE = 6
+    GUILD_MEMBER_JOIN = 7
+    USER_PREMIUM_GUILD_SUBSCRIPTION = 8
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_1 = 9
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2 = 10
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3 = 11
+    CHANNEL_FOLLOW_ADD = 12
+    GUILD_DISCOVERY_DISQUALIFIED = 14
+    GUILD_DISCOVERY_REQUALIFIED = 15
+    GUILD_DISCOVERY_GRACE_PERIOD_INITIAL_WARNING = 16
+    GUILD_DISCOVERY_GRACE_PERIOD_FINAL_WARNING = 17
+    THREAD_CREATED = 18
+    REPLY = 19
+    CHAT_INPUT_COMMAND = 20
+    THREAD_STARTER_MESSAGE = 21
+    GUILD_INVITE_REMINDER = 22
+    CONTEXT_MENU_COMMAND = 23
---
+++ somecord/user.py
@@ -0,0 +1,115 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2022-present EmreTech
+Copyright (c) 2022-present Astroicy
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+from typing import Optional, Union
+
+from discord_typings import UserData
+
+from .asset import Asset
+from .object import DiscordObject
+from .types.snowflake import *
+from .utils import MISSING, MissingType
+
+__all__ = ("User",)
+
+
+class User(DiscordObject):
+    """Represents a User type.
+
+    Attributes
+    ----------
+    id: :type:`Snowflake`
+        The id of this user
+    name: :type:`str`
+        The name of this user
+    discriminator: :type:`str`
+        The 4 digit tag for this user, also called a discriminator
+    avatar: :type:`Optional[Asset]`
+        The avatar for this user
+    bot: :type:`Union[MISSING, bool]`
+        If this user is a bot or not
+    tfa_enabled: :type:`Union[MISSING, bool]`
+        If this user has two-factor authentication or not
+    banner: :type:`Union[MISSING, None, Asset]`
+        The banner for this user
+    flags: :type:`Union[MISSING, int]`
+        The flags for this user
+    premium_type: :type:`Union[MISSING, int]`
+        If the user has Nitro, Nitro Classic, or none
+    public_flags: :type:`Union[MISSING, int]`
+        The public flags for this user
+    """
+
+    __slots__ = (
+        "id",
+        "name",
+        "discriminator",
+        "avatar",
+        "bot",
+        "tfa_enabled",
+        "accent_color",
+        "banner",
+        "flags",
+        "premium_type",
+        "public_flags",
+    )
+
+    def __init__(self, d: UserData, client):
+        DiscordObject.__init__(self, d, client)
+        self._update(d)
+
+    def _update(self, d: UserData):
+        self.id: Snowflake = d.get("id")
+        self.name: str = d.get("username")
+        self.discriminator: str = d.get("discriminator")
+        self.avatar: Optional[Asset] = (
+            Asset.from_user_avatar(self.client, self.id, d.get("avatar"))
+            if d.get("avatar")
+            else Asset.from_default_user_avatar(self.client, int(self.discriminator))
+        )
+        self.bot: Union[MissingType, bool] = d.get("bot", MISSING)
+        self.tfa_enabled: Union[MissingType, bool] = d.get("mfa_enabled", MISSING)
+        self.accent_color: Union[MissingType, Optional[int]] = MISSING
+        if d.get("accent_color", MISSING) is not MISSING:
+            self.accent_color = (
+                int(d.get("accent_color")) if d.get("accent_color") is not None else None
+            )
+        _banner_hash: Union[MissingType, Optional[str]] = d.get("banner", MISSING)
+        self.banner: Union[MissingType, Optional[Asset]] = MISSING
+        if _banner_hash is not MISSING:
+            self.banner = (
+                Asset.from_user_banner(self.client, self.id, _banner_hash)
+                if _banner_hash is not None
+                else None
+            )
+        # TODO: Locales
+        self.flags: Union[MissingType, int] = d.get("flags", MISSING)
+        self.premium_type: Union[MissingType, int] = d.get("premium_type", MISSING)
+        self.public_flags: Union[MissingType, int] = d.get("public_flags", MISSING)
+
+    @property
+    def mention(self) -> str:
+        """Returns a string that can mention this user."""
+        return f"<@{self.id}>"
